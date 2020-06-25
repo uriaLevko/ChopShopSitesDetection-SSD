d1676adbd944d521dc7ef5bd07a066a76c3f8493
Uria Levkovitz <urial@systematics.co.il>
2020-06-25 10:18:58 +0300

    Update layer_view.py

--- moduls/layer_view.py
+++ moduls/layer_view.py
@@ -1,5 +1,5 @@
-def Access_and_view_layer(layerName = '',colunmAnalyze = 'Classname'):
-    arcpy.env.workspace = r"C:\Users\urial\DeepLearningProjects\mashhetot\mashhetot.gdb"
+def Access_and_view_layer(layerName = '',colunmAnalyze = 'Classname',arc_env = ''):
+    arcpy.env.workspace = f'r"{arc_env}"'
     layer1 = GeoAccessor.from_featureclass(layerName)
     m1 = GIS().map(location = 'kalanswa',zoomlevel=11)
     layer1.spatial.plot(map_widget= m1,renderer_type='u',col=colunmAnalyze)
