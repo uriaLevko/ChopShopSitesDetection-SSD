960ead91b9dbaeda84510b0158a26fd6e38a31eb
Uria Levkovitz <urial@systematics.co.il>
2020-06-25 10:22:15 +0300

    Update layer_view.py

--- moduls/layer_view.py
+++ moduls/layer_view.py
@@ -1,7 +1,13 @@
 def Access_and_view_layer(layerName = '',colunmAnalyze = 'Classname',arc_env = ''):
+    """
+    sets as map and feature data.
+    return the layer and the map by: 
+    layer,map =  Access_and_view_layer(var1,var2,var3)
+    
+    """
     arcpy.env.workspace = f'r"{arc_env}"'
     layer1 = GeoAccessor.from_featureclass(layerName)
-    m1 = GIS().map(location = 'kalanswa',zoomlevel=11)
+    m1 = GIS().map(location = 'israel',zoomlevel=11)
     layer1.spatial.plot(map_widget= m1,renderer_type='u',col=colunmAnalyze)
     arcpy.ClearEnvironment("workspace")
     return(layer1,m1)
