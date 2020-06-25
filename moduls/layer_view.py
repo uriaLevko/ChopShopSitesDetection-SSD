939225ebe861be8199412e17c13b39455c46af73
Uria Levkovitz <urial@systematics.co.il>
2020-06-25 10:25:25 +0300

    Update layer_view.py

--- moduls/layer_view.py
+++ moduls/layer_view.py
@@ -1,10 +1,12 @@
-def Access_and_view_layer(layerName = '',colunmAnalyze = 'Classname',arc_env = ''):
+def Access_and_view_layer(layerName = '',colunmAnalyze = 'Classname',arc_env = '',online_url='https://xyz.maps.arcgis.com',user_name = 'xyz',password = 'xyz'):
     """
-    sets as map and feature data.
+    connects to agol.
+    sets a map and feature data.
     return the layer and the map by: 
     layer,map =  Access_and_view_layer(var1,var2,var3)
     
     """
+    gis = GIS(online_url,user_name,password)
     arcpy.env.workspace = f'r"{arc_env}"'
     layer1 = GeoAccessor.from_featureclass(layerName)
     m1 = GIS().map(location = 'israel',zoomlevel=11)
